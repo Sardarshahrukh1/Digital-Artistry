77d8ba256071cc2dd941571a481c1ad8b3045de4
Sardar Shahrukh <kshahrukh697@gmail.com>
2024-05-30 17:15:40 +0100

    menu data

---
+++ frontend/src/data/allMenu.js
@@ -0,0 +1,97 @@
+export const menu = [
+  {
+    name: "Find Friends",
+    icon: "friends",
+    description: "Search for friends or people you may know.",
+  },
+  {
+    name: "Friends",
+    icon: "friends",
+    description: "Connect with people who share your interests.",
+  },
+  {
+    name: "News Feed",
+    icon: "feed",
+    description: "See relevant posts from people and Pages you follow.",
+  },
+  {
+    name: "Pages",
+    icon: "pages",
+    description: "Discover and connect with businesses on Facebook.",
+  },
+  {
+    name: "Gaming Video",
+    icon: "gaming",
+    description: "Watch and connect with your favorite games and streamers.",
+  },
+  {
+    name: "Play Games",
+    icon: "play",
+    description: "Play your favorite games.",
+  },
+  {
+    name: "Notification",
+    icon: "Notification",
+    description:
+      "A video destination personalized to your interests and connections.",
+  },
+  {
+    name: "Pay",
+    icon: "pay",
+    description: "Pay.",
+  },
+  {
+    name: "Marketplace",
+    icon: "marketplace",
+    key: "marketplace",
+    description: "Buy and sell in your community.",
+  },
+  {
+    name: "Friend Request",
+    icon: "memories",
+    description: "Browse your old photos, videos and posts on Facebook.",
+  },
+  {
+    name: "Message",
+    icon: "Message",
+    description: "Find posts, photos and videos that you saved for later.",
+  },
+  {
+    name: "Weather",
+    icon: "weather",
+    description: "weather.",
+  },
+
+  {
+    name: "Jobs",
+    icon: "jobs",
+    description: "Find a job that's right for you.",
+  },
+  {
+    name: "Messenger",
+    icon: "messenger",
+    description: "Chat instantly with your friends and connections.",
+  },
+];
+export const create = [
+  {
+    name: "Post",
+    icon: "m_post_icon",
+  },
+  {
+    name: "Story",
+    icon: "m_story_icon",
+  },
+  {
+    name: "Room",
+    icon: "m_room_icon",
+  },
+  {
+    name: "Marketplace Listing",
+    icon: "m_post_mar",
+  },
+  {
+    name: "Job",
+    icon: "m_post_job",
+  },
+];
