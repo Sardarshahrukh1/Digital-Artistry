e2e9ae62a8b97a28754d85fa816fb62f42a33539
Sardar Shahrukh <kshahrukh697@gmail.com>
2024-05-30 16:23:52 +0100

    Create user.js

---
+++ backend/routes/user.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const {
+  register,
+  activateAccount,
+  login,
+  auth,
+  sendVerification,
+  findUser,
+  sendResetPasswordCode,
+  validateResetCode,
+  changePassword,
+  getProfile,
+  updateProfilePicture,
+  updateCover,
+  updateDetails,
+  addFriend,
+  cancelRequest,
+  follow,
+  unfollow,
+  acceptRequest,
+  unfriend,
+  deleteRequest,
+  search,
+  addToSearchHistory,
+  getSearchHistory,
+  removeFromSearch,
+  getFriendsPageInfos,
+  updateWallet,
+  getFriendsList,
+  loginForAdmin,
+  getAllUsers,
+  approveUser,
+  rejectUser,
+} = require("../controllers/user");
+const { authUser } = require("../middlwares/auth");
+
+const router = express.Router();
+
+router.get("/getProfile/:username", authUser, getProfile);
+router.get("/getAllUsers", authUser, getAllUsers);
+router.get("/getSearchHistory", authUser, getSearchHistory);
+router.get("/getFriendsPageInfos", authUser, getFriendsPageInfos);
+router.post("/register", register);
+router.post("/activate", authUser, activateAccount);
+router.post("/login", login);
+router.post("/admin-login", loginForAdmin);
+router.post("/sendVerification", authUser, sendVerification);
+router.post("/findUser", findUser);
+router.post("/sendResetPasswordCode", sendResetPasswordCode);
+router.post("/validateResetCode", validateResetCode);
+router.post("/changePassword", changePassword);
+router.post("/update-wallet", authUser, updateWallet);
+router.put("/updateProfilePicture", authUser, updateProfilePicture);
+router.put("/updateCover", authUser, updateCover);
+router.put("/updateDetails", authUser, updateDetails);
+router.put("/addFriend/:id", authUser, addFriend);
+router.put("/cancelRequest/:id", authUser, cancelRequest);
+router.put("/follow/:id", authUser, follow);
+router.put("/unfollow/:id", authUser, unfollow);
+router.put("/acceptRequest/:id", authUser, acceptRequest);
+router.put("/unfriend/:id", authUser, unfriend);
+router.put("/deleteRequest/:id", authUser, deleteRequest);
+router.post("/search/:searchTerm", authUser, search);
+router.put("/addToSearchHistory", authUser, addToSearchHistory);
+router.put("/removeFromSearch", authUser, removeFromSearch);
+router.put("/approveUser/:userId", authUser, approveUser);
+router.put("/rejectUser/:userId", authUser, rejectUser);
+
+module.exports = router;
