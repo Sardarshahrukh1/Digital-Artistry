6e65cdd0bfc942082e904438bbd19a16d32d8044
Sardar Shahrukh <kshahrukh697@gmail.com>
2024-05-30 16:10:08 +0100

    Create auth.js
    Add auth middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+//JWT is used for stateless authentication mechanisms for users and providers
+const jwt = require("jsonwebtoken");
+
+exports.authUser = async (req, res, next) => {
+  try {
+    let tmp = req.header("Authorization");
+
+    const token = tmp ? tmp.slice(7, tmp.length) : "";
+    if (!token) {
+      return res.status(400).json({ message: "Invalid Authentification" });
+    }
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        console.log("this is errir", err);
+        return res.status(400).json({ message: "Invalid Authentification" });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
