1cc5808ea02a8f0f99601749ad8f89bd554ec886
Sardar Shahrukh <kshahrukh697@gmail.com>
2024-05-30 17:15:59 +0100

    home data

---
+++ frontend/src/data/home.js
@@ -0,0 +1,52 @@
+export const left = [
+  {
+    text: "Find friends",
+    img: "find",
+    key: "friends/suggestions",
+  },
+  {
+    text: "Friend Request",
+    img: "add-friends",
+    key: "friends/requests",
+  },
+  {
+    text: "Friends",
+    img: "friends",
+    key: "friends",
+  },
+  {
+    text: "Marketplace",
+    img: "marketplace-lft",
+    key: "marketplace",
+  },
+  {
+    text: "Buying",
+    img: "buy",
+    key: "marketplace/buying",
+  },
+  {
+    text: "Selling",
+    img: "sell",
+    key: "marketplace/selling",
+  },
+  {
+    text: "Message",
+    img: "messenger-lft",
+    key: "messenger",
+  },
+  {
+    text: "Wallet",
+    img: "wallet",
+    key: "wallet",
+  },
+  {
+    text: "Validator",
+    img: "validator",
+    key: "validator",
+  },
+  {
+    text: "Log Out",
+    img: "logout",
+    key: "logout",
+  },
+];
