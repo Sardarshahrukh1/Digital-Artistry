bdbc0c4581961bce16e75746b34a7e140e5e7e2c
Sardar Shahrukh <kshahrukh697@gmail.com>
2024-05-30 17:13:54 +0100

    Profile pages

---
+++ frontend/src/pages/profile/GridPosts.js
@@ -0,0 +1,13 @@
+export default function GridPosts() {
+  return (
+    <div className="createPost">
+      <div
+        className="createPost_header"
+        style={{ justifyContent: "space-between" }}
+      >
+        <div className="left_header_grid">Posts</div>
+      </div>
+      <div className="create_splitter"></div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/profile/PplYouMayKnow.js
@@ -0,0 +1,6 @@
+import { Dots } from "../../svg";
+import { stories } from "../../data/home";
+import AddFriendSmallCard from "./AddFriendSmallCard";
+export default function PplYouMayKnow() {
+  return <span></span>;
+}
